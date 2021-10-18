280b6abf12c6d79802945d5dd3a6b143f0a95236
肖连伟 <2695772510@qq.com>
2021-10-18 15:20:29 +0800

    测试

--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -1,1 +1,18 @@
-歌手
+<template>
+  <div class="singer wrapper">
+    歌手<el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.singer {
+  height: 200px;
+  background: red;
+}
+</style>
